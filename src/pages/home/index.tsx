d58dd14a6ae417856e467ba7b6640966ee10d3ab
Oatmealcake <rlarhdms26@gmail.com>
2024-03-08 17:43:06 +0900

    update: 4-2 게시글 폼

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -69,10 +69,12 @@ export default function HomePage() {
 
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
       </div>
       <PostForm />
       <div className="post">
